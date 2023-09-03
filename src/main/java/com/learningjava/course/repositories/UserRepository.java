11e9eb3bd2ba5eadf691a7c1d36ebcb7181142d0
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2023-09-03 18:08:14 +0100

    Service Layer

--- src/main/java/com/learningjava/course/repositories/UserRepository.java
+++ src/main/java/com/learningjava/course/repositories/UserRepository.java
@@ -3,5 +3,6 @@ package com.learningjava.course.repositories;
 import com.learningjava.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
