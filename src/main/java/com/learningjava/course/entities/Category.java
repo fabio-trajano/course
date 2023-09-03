43570b0ee2e172d6f41b424eb3685da6626ce91a
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2023-09-03 21:05:21 +0100

    Product

--- src/main/java/com/learningjava/course/entities/Category.java
+++ src/main/java/com/learningjava/course/entities/Category.java
@@ -4,7 +4,10 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
@@ -14,6 +17,8 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category() {}
     public Category(Long id, String name) {
@@ -35,6 +40,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
